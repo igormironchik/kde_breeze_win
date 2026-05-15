6a11438a632e87f3b6cad63605feac7efd1dbbd4
Igor Mironchik <igor.mironchik@gmail.com>
2026-05-15 16:47:01 +0300

    Make initial commit.

---
+++ clean.bat
@@ -0,0 +1,8 @@
+rmdir /S /Q Qt
+del /Q Markdown_Windows_x64.Installer.exe
+del /Q aqtinstall.log
+rmdir /S /Q installer\packages\mironchik.igor.markdown\data\bin
+rmdir /S /Q installer\packages\mironchik.igor.markdown\data\lib
+rmdir /S /Q installer\packages\mironchik.igor.markdown\data\plugins
+rmdir /S/ Q installer\packages\mironchik.igor.markdown\data\resources
+rmdir /S /Q installer\packages\mironchik.igor.markdown\data\translations
---
+++ conanfile.py
@@ -0,0 +1,29 @@
+from conan import ConanFile
+
+class DependenciesRecipe(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps", "PkgConfigDeps"
+
+    def requirements(self):
+        self.requires("libiconv/1.17")
+        if self.settings.os == "Windows":
+            self.requires("gettext/1.0")
+            self.requires("libgettext/0.26")
+            self.tool_requires("winflexbison/2.5.25")
+            self.requires("zstd/1.5.5")
+            self.requires("bzip2/1.0.8")
+            self.requires("xz_utils/5.8.3")
+            self.requires("libxslt/1.1.45")
+
+    def configure(self):
+        self.options["libiconv"].shared = False
+        self.options["gettext"].shared = False
+        self.options["libgettext"].shared = False
+        self.options["zstd"].shared = False
+        self.options["bzip2"].shared = False
+        self.options["xz_utils"].shared = False
+        self.options["libxslt"].shared = False
+
+    def imports(self):
+        self.copy("*.dll", "bin", "bin")
+        self.copy("*.dylib", "lib", "lib")
